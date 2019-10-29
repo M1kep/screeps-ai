2c7687e8d351bce58652fded7e092dd3763f5243
Michael Poutre <m1kep.my.mail@gmail.com>
2019-10-28 18:05:33 -0700

    Server side updates.

--- prototype.spawn.js
+++ prototype.spawn.js
@@ -6,6 +6,9 @@ module.exports = {
     StructureSpawn.prototype.createCustomCreep = function (energy, role) {
       const numParts = Math.floor(energy / 200)
       var body = []
+      if(numParts === 1) {
+          body.push(MOVE)
+      }
       for (let i = 0; i < numParts; i++) {
         body.push(WORK)
       }
